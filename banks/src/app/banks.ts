8f426d03a99392060fe9e7e198d9fce546c2a6a8
perrotth1 <perrotth1@hawkmail.newpaltz.edu>
2022-05-12 15:24:16 -0400

    did components exercise

---
+++ banks/src/app/banks.ts
@@ -0,0 +1,7 @@
+import { Bank } from './bank';
+export const BANKS: Bank[] = [
+{ id: 11, name: 'Bank of America Merrill Lynch', rating: '6/10', logo: 'https://mlaem.fs.ml.com/content/dam/ML/global/defaultSocialImage.png' },
+{ id: 12, name: 'Barclays Capital', rating: '7/10', logo: 'https://www.cib.barclays/content/dam/barclaysmicrosites/ibpublic/Images/Test/BAR_Flat_Landscape_1C_.jpg' },
+{ id: 13, name: 'Citi', rating: '5/10', logo: 'https://pentagram-production.imgix.net/wp/592ca89f19a1d/ps-citibank-01.jpg?rect=0%2C177%2C960%2C600&w=880&fit=crop&fm=jpg&q=70&auto=format&h=548' },
+
+];
